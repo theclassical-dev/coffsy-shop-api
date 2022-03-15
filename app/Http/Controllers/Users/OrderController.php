5eba650bc7f8a410834c197b923637d72c3e80ec
Abraham Peter <theclassical001@gmail.com>
2022-03-15 12:08:13 +0100

    order check + order request function added

--- app/Http/Controllers/Users/OrderController.php
+++ app/Http/Controllers/Users/OrderController.php
@@ -4,8 +4,60 @@ namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Order;
 
 class OrderController extends Controller
 {
-    //
+    public function index(Request $request){
+
+       $order =  auth()->user()->order;
+        if(!$order->isEmpty()){
+
+            return response()->json([
+                'data' => $order
+            ]);
+        }
+
+        return response()->json(['message' => 'no order available']);
+       
+    }
+
+    public function order(Request $request){
+        $request->validate([
+            'tea_type' => 'required',
+            'description' => 'required',
+            'address' => 'required',
+            'promo_code' => 'nullable',
+            'pay_type' => 'required',
+            'pay_confirmation' => 'required',
+            'status' => 'required',
+        ]);
+
+        $name = $request->input('firstName').' '.$request->input('lastName');
+        $n = auth()->user()->firstName.' '.auth()->user()->lastName;
+
+        $order = Order::create([
+            'name' => $n,
+            'coff_id' => auth()->user()->coff_id,
+            'tea_type' => $request->input('tea_type'),
+            'description' => $request->input('description'),
+            'address' => $request->input('address'),
+            'promo_code' => $request->input('promo_code'),
+            'pay_type' => $request->input('pay_type'),
+            'pay_confirmation' => $request->input('pay_confirmation'),
+            'status' => $request->input('status')
+        ]);
+
+        if($roder){
+            return response()->json([
+                'message' => 'Order Successful Created, Processing...',
+                'data' => $order
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Order Failed'
+        ]);
+    }
 }
