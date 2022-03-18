54d024e11b88fe8ba4949a3603a34a5f1e711beb
Abraham Peter <theclassical001@gmail.com>
2022-03-18 12:59:25 +0100

    yearlyReport model + migration created + updated

---
+++ app/Models/YearlyReport.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class YearlyReport extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['delivered','cash','transfer','total','year','submittedDate'];
+
+}
