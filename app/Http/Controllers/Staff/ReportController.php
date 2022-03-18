0b862335dfb54b180e7b858eb5b18122bf1b3e46
Abraham Peter <theclassical001@gmail.com>
2022-03-18 12:37:23 +0100

    reports controller + models created

---
+++ app/Http/Controllers/Staff/ReportController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Staff;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\Order;
+use Carbon\Carbon;
+
+class ReportController extends Controller
+{
+    //
+}
