b321037629d2cbeea4e6add7fd5af42d07a9ecaa
Abraham Peter <theclassical001@gmail.com>
2022-03-18 13:19:34 +0100

    report function created

--- app/Http/Controllers/Staff/ReportController.php
+++ app/Http/Controllers/Staff/ReportController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Models\Order;
-use App\Models\Report;
 use App\Models\DailyReport;
 use App\Models\WeeklyReport;
 use App\Models\MonthlyReport;
@@ -14,5 +13,21 @@ use Carbon\Carbon;
 
 class ReportController extends Controller
 {
-    //
+    public function report(Request $request){
+        
+        $request->validate([
+            'delivered' => 'required',
+            'cash' => 'required',
+            'trasfer' => 'required',
+            'total' => 'required',
+        ]);
+
+        $report = DailyReport::create([
+            'delivered' => $report->input('delivered'),
+            'cash' => $report->input('cash'),
+            'trasfer' => $report->input('trasfer'),
+            'total' => $report->input('total'),
+            'submittedDate' => Carbon::now('WAT')->format('l jS \of F Y h:i:s')
+        ]);
+    }
 }
